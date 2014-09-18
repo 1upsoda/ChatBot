dce3fb75b2006fd03ffde1f366918ff4ae18a7d2
1upsoda <1upsoda@gmail.com>
2014-09-18 09:50:54 -0600

    Extra
    I made some more stuff

--- src/chat/controller/Continue.java
+++ src/chat/controller/Continue.java
@@ -13,13 +13,24 @@ public class Continue
 	private void ContinueGUI()
 	{
 		String answerGUI = JOptionPane.showInputDialog("What do you want to chat aboot?");
-		if(answerGUI.equalsIgnoreCase("Ponies") || answerGUI.equalsIgnoreCase("Pones") || answerGUI.equalsIgnoreCase("Pony") || answerGUI.equalsIgnoreCase("Pone"));
+		if(answerGUI.equalsIgnoreCase("Ponies") || answerGUI.equalsIgnoreCase("Pones") || answerGUI.equalsIgnoreCase("Pony") || answerGUI.equalsIgnoreCase("Pone"))
 		{
-			JOptionPane.showMessageDialog(null, "Then why the heck did you play with The Chat%20Bot");
+			JOptionPane.showMessageDialog(null, "OOh, what's your favorite Pony?");
+			PonyChat myPonyChat = new PonyChat();
+			myPonyChat.start();
+			
 		}
-		if(answerGUI.equalsIgnoreCase("ponys"))
+		else if(answerGUI.equalsIgnoreCase("Pwny") || answerGUI.equalsIgnoreCase("Pwnies") || answerGUI.equalsIgnoreCase("PWN13Z") || answerGUI.equalsIgnoreCase("PWNIEZ") || answerGUI.equalsIgnoreCase("PONIEZ") || answerGUI.equalsIgnoreCase("pon13z"))
 		{
-			JOptionPane.showMessageDialog(null, "Then why the heck did you play with The Chat%20Bot");
+			JOptionPane.showMessageDialog(null, "00h, wh@1'$ Y0R F@V0R1T3 0f teh PWN13Z?");
+			PonyChat myPonyChat = new PonyChat();
+			myPonyChat.start();
+		}
+		else if(answerGUI.equalsIgnoreCase("ponys"))
+		{
+			JOptionPane.showMessageDialog(null, "I mean, good try at english but... Whatever, what is your favorite pony?");
+			PonyChat myPonyChat = new PonyChat();
+			myPonyChat.start();
 		}
 	}
 }
--- src/chat/controller/PonyChat.java
+++ src/chat/controller/PonyChat.java
@@ -3,4 +3,17 @@ package chat.controller;
 public class PonyChat
 {
 
+	public void start()
+	{
+		ponyGUI();
+		
+	}
+	private void ponyGUI()
+	{
+		String answerGUI 
+		
+		
+		
+		
+	}
 }
